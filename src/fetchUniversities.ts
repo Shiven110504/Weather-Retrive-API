fb453c5cf007e96ed53c0ac83443aaef56b14820
Shiven Patel <shiven110504@gmail.com>
2023-11-19 00:19:31 -0500

    Initial Commit

---
+++ src/fetchUniversities.ts
@@ -0,0 +1,22 @@
+import fetch from "../include/fetch.js";
+
+export function fetchUniversities(query: string): Promise<string[]> {
+  // TODO
+  const url = `http://220.maxkuechen.com/universities/search?name=${query}`;
+  return fetch(url)
+    .then((response) => { 
+      if(!response.ok) {
+        throw new Error(`Failed to fetch universities: ${response.statusText}`)
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if(!Array.isArray(data)) {
+        return [];
+      }
+        return data.map((university) => university.name);
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+}
